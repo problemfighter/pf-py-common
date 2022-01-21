8f3e482741cb9c36182047885c6388180af5dcf9
hmtmcse <hmtm.cse@gmail.com>
2022-01-22 00:51:01 +0600

    import class from string

--- pf_py_common/py_common.py
+++ pf_py_common/py_common.py
@@ -1,4 +1,6 @@
+import sys
 import uuid
+from pf_py_common.pf_exception import PFException
 
 
 class PyCommon:
@@ -7,3 +9,30 @@ class PyCommon:
     def uuid() -> str:
         unique_id = str(uuid.uuid1())
         return unique_id.upper()
+
+    @staticmethod
+    def import_from_string(import_name: str, silent: bool = False):
+        import_name = import_name.replace(":", ".")
+        try:
+            try:
+                __import__(import_name)
+            except ImportError:
+                if "." not in import_name:
+                    raise
+            else:
+                return sys.modules[import_name]
+
+            module_name, obj_name = import_name.rsplit(".", 1)
+            module = __import__(module_name, globals(), locals(), [obj_name])
+            try:
+                return getattr(module, obj_name)
+            except AttributeError as e:
+                raise ImportError(e)
+
+        except ImportError as e:
+            if not silent:
+                error = "Emport Name: " + import_name
+                error += "\n" + str(e)
+                raise PFException(error)
+
+        return None
