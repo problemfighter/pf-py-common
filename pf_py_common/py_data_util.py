db5598ca5f977fa58aaa0cbcad4218df248431e0
hmtmcse <hmtm.cse@gmail.com>
2022-07-01 09:42:31 +0600

    added class attrs extractor

--- pf_py_common/py_data_util.py
+++ pf_py_common/py_data_util.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class PyDataUtil:
 
     @staticmethod
@@ -33,3 +36,13 @@ class PyDataUtil:
                 if hasattr(source_object, field):
                     setattr(dst_object, field, getattr(source_object, field))
         return dst_object
+
+    @staticmethod
+    def get_class_attrs(class_or_object) -> dict:
+        name_and_type = {}
+        for attrs in inspect.getmembers(class_or_object):
+            if len(attrs) == 2 and attrs[0] == "__annotations__":
+                for attr_name in attrs[1]:
+                    name_and_type[attr_name] = attrs[1][attr_name].__name__
+                break
+        return name_and_type
